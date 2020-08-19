a8f8d2542069258b760c8e7f92790a0e37659a2d
=Bruno Igor <=brigor7@gmail.com>
2020-08-19 15:56:49 -0300

    Create migration and model User

---
+++ backend/src/models/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  create_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
+
+export default User;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,11 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (_, res) => res.send({ Message: 'Server started!' }));
+routes.get('/', (_, res) =>
+  res.send({ Message: 'Server Match_Renascer Started!' }),
+);
 routes.post('/', (req, res) => {
   const user = req.body;
+  res.json(user);
 });
 export default routes;
